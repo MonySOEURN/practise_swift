cecc6f1be61f63ddef840490e4e638dd97b0fc98
Mony SOEURN <soeurn.mony@gmail.com>
2020-02-28 14:58:28 +0700

    [Initial] Kick start project

---
+++ ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youTube-CodeWithChris_app
+//
+//  Created by Mony SOEURN on 2/1/2563 BE.
+//  Copyright © 2563 BE Mony SOEURN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
